8ef50696ea59c453d72de540f2ed7f95e32a99bf
josueraoult <103897395+josueraoult@users.noreply.github.com>
2024-02-23 19:59:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ import uvicorn
 import httpx
 import os
 
-
 class WebhookRequestData(BaseModel):
     object: str = ""
     entry: List = []
 
-
-async def send_message(
+def send_message(
     page_access_token: str,
     recipient_id: str,
     message_text: str,
@@ -29,38 +27,25 @@ async def send_message(
     )
     r.raise_for_status()
 
-
-""" 
-Init FastAPI app and define endpoints.
-"""
 app = FastAPI()
-
 
 # Endpoints.
 @app.get("/api/webhook")
 async def verify(request: Request):
-    """
-    On webook verification VERIFY_TOKEN has to match the token at the
-    configuration and send back "hub.challenge" as success.
-    """
     if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get(
         "hub.challenge"
     ):
         if (
             not request.query_params.get("hub.verify_token")
-            == os.environ["2c361288a69c0c40ef760e2c4aa007f4"]
+            == os.environ.get("2c361288a69c0c40ef760e2c4aa007f4")
         ):
             return Response(content="Verification token mismatch", status_code=403)
         return Response(content=request.query_params["hub.challenge"])
 
     return Response(content="Required arguments haven't passed.", status_code=400)
 
-
 @app.post("/api/webhook")
 async def webhook(data: WebhookRequestData):
-    """
-    Messages handler.
-    """
     if data.object == "page":
         for entry in data.entry:
             messaging_events = [
@@ -70,20 +55,16 @@ async def webhook(data: WebhookRequestData):
                 message = event.get("message")
                 sender_id = event["sender"]["id"]
 
-                await send_message(
-                    page_access_token=os.environ["EAAJ3opZCZBh4MBO4PQgzmxYllgslFvVXYsZApczTQWnfYAZCUXpAF1HZBiw7dZAovZBBCvAx9wCZB5J8AFq9Xtx2xCM4j2sQULYRFbUnt5Wdt9wbB6ajVA0SWs1y0sgtdE84L9tsMxv7WOmS2WoRTab4pSZBUgWSDv1hwg42BTfwLQsfbxGlmIZBwe6tPjN0R6FPqe"],
+                send_message(
+                    page_access_token=os.environ.get("EAAJ3opZCZBh4MBO4PQgzmxYllgslFvVXYsZApczTQWnfYAZCUXpAF1HZBiw7dZAovZBBCvAx9wCZB5J8AFq9Xtx2xCM4j2sQULYRFbUnt5Wdt9wbB6ajVA0SWs1y0sgtdE84L9tsMxv7WOmS2WoRTab4pSZBUgWSDv1hwg42BTfwLQsfbxGlmIZBwe6tPjN0R6FPqe"),
                     recipient_id=sender_id,
                     message_text=f"Received: {message['text']}",
                 )
 
     return Response(content="ok")
 
-
 def main():
-    if "VERIFY_TOKEN" in os.environ:
-        print("your verify token is: ", os.environ["2c361288a69c0c40ef760e2c4aa007f4"])
     uvicorn.run(app=app)
-
 
 if __name__ == "__main__":
     main()
