56a0db3fcf27cf80b49c30e25bc072606cbadf36
josueraoult <103897395+josueraoult@users.noreply.github.com>
2024-02-25 21:18:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ WEBHOOK_VERIFY_TOKEN = 'nakama'
 ACCESS_TOKEN = 'EAAPPGL8wMRIBOx3b6igx3apEa6EDOXsqGzGN7mgDkQDOE8ZAZBBbSCVqAAzyTVJZAEZBYOCWYjaQvSJ0s3WGfMIOnqEUclrQEz6CjvwBHAhzyXlZABZANq4fSZBeYTS91AZASmJZA5cBYCRXQeBTf0xZCmBfstIbrHXh2J5AHZCVZCZBkOI4vP00DcnL8AtSUMAkHyJ8p'  # Remplacez par votre propre token
 
 # Configurez votre clé d'API pour OpenAI GPT-3.5-turbo
-OPENAI_API_KEY = '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'  # Remplacez par votre propre clé
+OPENAI_API_KEY = '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'  # Remplacez par votre propre clé
 
 # Initialisez le client pymessenger
 bot = Bot(ACCESS_TOKEN)
